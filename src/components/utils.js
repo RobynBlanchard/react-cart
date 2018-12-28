3a075ca97dad09fa622c78fb1e0541593bb8e9b2
Robyn <robynblanchard@notonthehighstreet.com>
2018-12-28 21:46:00 +0000

    Add comma to price when greater or equal to 1000

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,4 +1,7 @@
 export function printPrice(price) {
   const fractionalPrice = price / 100;
-  return `£${fractionalPrice.toFixed(2)}`;
+  return `£${fractionalPrice
+    .toFixed(2)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
 }
