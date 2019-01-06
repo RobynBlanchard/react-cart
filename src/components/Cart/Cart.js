f65a679d028c2ef8187aa57ba9eee29ace08f812
Robyn <robynblanchard@notonthehighstreet.com>
2019-01-06 21:03:59 +0000

    Use reduce to calculate sum of cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import CartList from './CartList/CartList';
-import CartSummary from './CartSummary/CartSummary';
+import React from "react";
+import CartList from "./CartList/CartList";
+import CartSummary from "./CartSummary/CartSummary";
 
 class Cart extends React.Component {
-
   updateCart = (product, operation) => {
-    this.props.updateCart(product,operation);
-  }
+    this.props.updateCart(product, operation);
+  };
 
   totalPrice = () => {
-    let sum = 0;
-    this.props.items.forEach(item => {
-      sum += item.product.price * item.quantity
-    });
-    return sum
-  }
+    return this.props.items.reduce(function(prev, cur) {
+      return prev + (cur.product.price * cur.quantity);
+    }, 0);
+  };
 
   render() {
-    return(
+    return (
       <div className="whole-cart">
-        <CartList items={this.props.items} updateCart={this.updateCart}/>
+        <CartList items={this.props.items} updateCart={this.updateCart} />
         <CartSummary totalPrice={this.totalPrice()} />
       </div>
-    )
+    );
   }
 }
 
