be39fba44fed9ff7228e0c79b93ecc10ccbb0ed7
Robyn <robynblanchard@notonthehighstreet.com>
2019-01-06 21:49:21 +0000

    Refactor app.js store cart total price  as state

--- src/components/Common/App.js
+++ src/components/Common/App.js
@@ -8,59 +8,69 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends React.Component {
   state = {
-    cart: []
+    cart: [],
+    totalPrice: 0
   };
 
-  updateCart = (product, operation = ">") => {
-    this.setState(prevState => {
-      let updatedCart = prevState.cart;
-
-      let index = prevState.cart
-        .map(function(e) {
-          return e.product;
-        })
-        .indexOf(product);
-
-      if (index === -1) {
-        updatedCart.push({ product: product, quantity: 1 })
-      } else {
-        if (operation === "<" && updatedCart[index].quantity === 1) {
-          updatedCart.splice(index, 1);
-        } else {
-          updatedCart[index] = {
-            product,
-            quantity: updatedCart[index].quantity + (operation === "<" ? -1 : 1)
-          };
-        }
-      }
-
-      return { cart: updatedCart };
-    });
-  };
-
-  quantityOfProducts = () => {
-    if (this.state.cart.length === 0) {
+  quantityOfProducts = cart => {
+    if (cart.length === 0) {
       return 0;
     }
-    const quantityObj = this.state.cart.reduce((a, b) => ({
+    const quantityObj = cart.reduce((a, b) => ({
       quantity: a.quantity + b.quantity
     }));
     return quantityObj.quantity;
+  };
+
+  updateCartItems = (prevState, product, operation) => {
+    let updatedCart = prevState.cart;
+
+    let index = prevState.cart
+      .map(function(e) {
+        return e.product;
+      })
+      .indexOf(product);
+
+    if (index === -1) {
+      updatedCart.push({ product: product, quantity: 1 });
+    } else {
+      if (operation === "<" && updatedCart[index].quantity === 1) {
+        updatedCart.splice(index, 1);
+      } else {
+        updatedCart[index] = {
+          product,
+          quantity: updatedCart[index].quantity + (operation === "<" ? -1 : 1)
+        };
+      }
+    }
+    
+    return updatedCart;
+  };
+
+  handleCartUpdate = (product, operation = ">") => {
+    this.setState(prevState => {
+      const updatedCart = this.updateCartItems(prevState, product, operation);
+
+      return {
+        cart: updatedCart,
+        totalPrice: this.quantityOfProducts(updatedCart)
+      };
+    });
   };
 
   render() {
     return (
       <Router>
         <div className="ui container" id="app">
-          <NavBar productCount={this.quantityOfProducts()} />
+          <NavBar productCount={this.state.totalPrice} />
           <Route
             path="/products"
-            render={() => <ProductList updateCart={this.updateCart} />}
+            render={() => <ProductList updateCart={this.handleCartUpdate} />}
           />
           <Route
             path="/cart"
             render={() => (
-              <Cart items={this.state.cart} updateCart={this.updateCart} />
+              <Cart items={this.state.cart} updateCart={this.handleCartUpdate} />
             )}
           />
         </div>
